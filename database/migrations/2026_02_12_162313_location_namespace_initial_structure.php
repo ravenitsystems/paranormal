d3e49f7c2fba501cd1e25ee0bc7aecf213be9406
Nicholas Ashley <ravenitsystems@outlook.com>
2026-02-12 16:32:40 +0000

    Repaired defects in the location namespace and removed default tables

--- database/migrations/2026_02_12_162313_location_namespace_initial_structure.php
+++ database/migrations/2026_02_12_162313_location_namespace_initial_structure.php
@@ -9,34 +9,34 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('location_country', function (Blueprint $table) {
-            $table->bigInteger('id')->unsigned()->autoIncrement()->primary();
+            $table->bigInteger('id')->unsigned()->autoIncrement();
             $table->string('name', 64);
             $table->string('code', 4);
         });
 
         Schema::create('location_state', function (Blueprint $table) {
-            $table->bigInteger('id')->unsigned()->autoIncrement()->primary();
+            $table->bigInteger('id')->unsigned()->autoIncrement();
             $table->string('name', 64);
             $table->bigInteger('country_id')->unsigned();
             $table->foreign('country_id')->references('id')->on('location_country');
         });
 
         Schema::create('location_county', function (Blueprint $table) {
-            $table->bigInteger('id')->unsigned()->autoIncrement()->primary();
+            $table->bigInteger('id')->unsigned()->autoIncrement();
             $table->string('name', 64);
             $table->bigInteger('state_id')->unsigned();
             $table->foreign('state_id')->references('id')->on('location_state');
         });
 
         Schema::create('location_town', function (Blueprint $table) {
-            $table->bigInteger('id')->unsigned()->autoIncrement()->primary();
+            $table->bigInteger('id')->unsigned()->autoIncrement();
             $table->string('name', 64);
             $table->bigInteger('county_id')->unsigned();
             $table->foreign('county_id')->references('id')->on('location_county');
         });
 
         Schema::create('location_exact', function (Blueprint $table) {
-            $table->bigInteger('id')->unsigned()->autoIncrement()->primary();
+            $table->bigInteger('id')->unsigned()->autoIncrement();
             $table->bigInteger('longitude');
             $table->bigInteger('latitude');
             $table->bigInteger('town_id')->unsigned();
